c53265298408ad3b653e3ed991dc1732f8b4b144
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2021-12-19 02:20:22 +0530

    Login Endpoints for dealing with tokens

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -50,8 +50,49 @@ router.post('/createuser', [
 
   } catch (error) {
       console.log(error.message);
-      res.status(500).send("Some error ocurred");
+      res.status(500).send("Internsl Server Error");
   }
 })
+
+//Authenticate a User using: POST "/api/auth/login". No Login Required
+
+router.post('/login', [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password cannot be blank').exists(),
+], async (req, res)=>{
+
+  //If there are errors, return bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {email, password} = req.body;
+  try {
+    let user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({error: "Please try to login with correct credentials"});
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if(!passwordCompare){
+      return res.status(400).json({error: "Please try to login with correct credentials"});
+    }
+
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({authtoken});
+
+  } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internsl Server Error");
+  }
+
+})
+
 
 module.exports = router
