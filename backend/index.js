753c944424779b9aff36cd2dc84b11bc56fbb776
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2021-12-18 16:33:18 +0530

    Creating Routes and Schema

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const connectToMongo = require('./db');
+const express = require('express')
+connectToMongo();
+const app = express()
+const port = 3000
+
+//Available Routes
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+app.get('/', (req, res) => 
+  res.send('Hello VatsAL!')
+),
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
