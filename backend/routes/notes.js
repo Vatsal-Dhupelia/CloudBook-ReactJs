a24f5995348339448043bb5ed7068a9662de096a
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2021-12-19 11:04:40 +0530

    Backend Code for Deleting a Note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -48,29 +48,64 @@ router.post(
   }
 );
 
-//ROUTE 3 : Update an existing Note: POST "/api/notes/updatenote". Login Required
-router.put(
-    "/updatenote/:id",
-    fetchuser,  
-    async (req, res) => {
-        const{title, description, tag} = req.body;
-        //Create a newNote object
-        const newNote = {};
-        if(title){newNote.title = title};
-        if(description){newNote.description = description};
-        if(tag){newNote.tag = tag};
+//ROUTE 3 : Update an existing Note: PUT "/api/notes/updatenote". Login Required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  try {
+    //Create a newNote object
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
 
-        //Find the Note to be updated and update it
-        let note = await Note.findById(req.params.id);
-        if(!note){return res.status(404).send("Not Found")}
+    //Find the Note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
 
-        if(note.user.toString() !== req.user.id){
-            return res.status(401).send("Not Allowed");
-        }
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
 
-        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
-        res.json({note}); 
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json({ note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internsl Server Error");
+  }
+});
 
-    })
+//ROUTE 4 : Delete an existing Note: DELETE "/api/notes/deletenote". Login Required
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  try {
+    //Find the Note to be deleted and delete it
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+
+    //Allow deletion only if user owns this note
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note has been deleted", note: note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internsl Server Error");
+  }
+});
 
 module.exports = router;
